db70ca6b4e48882408e462bdd611c4fd304b74af
Samuel Nelson <samuel.h.nelson@protonmail.com>
2023-07-02 17:56:24 -0500

    finished part 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,8 +1,49 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
 
+//Add Items
+function addItem(item) {
+basket.unshift(item);
+for (let i = 0; i < basket.length; i++) {
+    if (basket[i]==item){
+      return true;
+    }
+    else if(basket[i] != item) {
+      return false;
+    }
+  }
+}
+console.log(addItem('icecream'));
+console.log(addItem('pizza'));
+console.log(basket);
 
+//List Items
+function listItems(){
+    for (each of basket) {
+        console.log(each);
+    }
+}
+console.log(listItems());
+
+//Empty Basket
+function empty() {
+    basket = [];
+}
+empty();
+console.log(basket);
+
+//Max Items
+const maxItems = 5;
+function isFull(){
+    if (maxItems > basket.length){
+        return false;
+    }
+    else if (maxItems <= basket.length){
+        return true;
+    }
+}
 
 
 
